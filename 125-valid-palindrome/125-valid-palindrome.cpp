c132eab3ea7a1a254ad39d95c426a6d4067a4246
Nikhil Chhipa <chhipanikhil9@gmail.com>
2022-08-18 18:43:22 +0530

    Time: 5 ms (79.25%), Space: 7.4 MB (77.63%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp;
         int n = s.size();
-        for(int i=0;i<n;i++){
-            if((('a'<=s[i] and s[i]<='z') or ('A'<=s[i] and s[i]<='Z')) or ('0'<=s[i] and s[i]<='9')){
-                temp += tolower(s[i]);
-            }
-        }
-        cout<<temp;
-        int i=0,j = temp.size()-1;
+        int i=0,j = n-1;
         while(i<j){
-            if(temp[i]!=temp[j]){
+            while(!isalnum(s[i]) and i<j){
+                i++;
+            }
+            while(!isalnum(s[j]) and i<j){
+                j--;
+            }
+            if(tolower(s[i]) != tolower(s[j])){
                 return false;
             }
             i++,j--;
